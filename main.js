0bb4b8979d70c6cfd68ec9cce331a55c239cd307
plantronix <98648582+plantronix@users.noreply.github.com>
2023-09-04 19:36:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+noseX=0;
+noseY=0;
+function preload() {
+    mustache = loadImage('https://i.postimg.cc/kXMpJ1XY/mustache.png');
+
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('poseNet Is Initialised');
+}
+
+function draw() {
+image(video, 0, 0, 300, 300);
+fill(255,0,0);
+stroke(255,0,0);
+image(mustache, noseX, noseY, 30, 30);
+}
+
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
+function gotPoses(results) 
+{
+    if(results.length > 0)
+    {
+
+        console.log(results);
+        noseX = results[0].pose.nose.x-20;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
